98c0d5b2a654413b261496e5c02711e0de7c081d
Udara Wickramasinghe <65730273+WickyRR@users.noreply.github.com>
2024-06-12 16:20:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const cors = require('cors')
-
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('chemibot_db');
 const app = express()
-const port = 4000
+const port = 8000
 
+
+
+db.serialize(() => {
+    db.run("CREATE TABLE lorem (info TEXT)");
+
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
+
+    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
+    });
+});
+
+db.close();
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
