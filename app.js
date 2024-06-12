c9d96fd735c46219fb4079f8572b8cf3c15b973b
Anuhas Paranavithana <anuhasindumina@gmail.com>
2024-06-12 16:56:11 +0530

    update db

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.sqlite');
 const app = express()
-const port = 4000
+const port = 8000
 
 
 
-db.serialize(() => {
-    db.run("CREATE TABLE chemicals (c_id int NOT NULL,rfid VARCHAR(64),name VARCHAR(64),row INT(1),col INT(1),presence TINYINT(1),PRIMARY KEY(c_id))");
-    db.run("CREATE TABLE selected_items (c_id int(3),PRIMARY KEY(c_id))");
+// db.serialize(() => {
+//     db.run("CREATE TABLE chemicals (c_id int NOT NULL,rfid VARCHAR(64),name VARCHAR(64),row INT(1),col INT(1),presence INT(1),PRIMARY KEY(c_id))");
+//     db.run("CREATE TABLE selected_items (c_id int(3),PRIMARY KEY(c_id))");
 
-    const stmt = db.prepare("INSERT INTO chemicals (c_id,rfid,name,row,col,presence) VALUES (1,'rfidnacl','NaCl',1,1,1),(2,'rfidnaoh','NaOH',1,2,1)");
-    //for (let i = 0; i < 10; i++) {
-    //    stmt.run("Ipsum " + i);
-    //}
-    //stmt.finalize();
+//     const stmt = db.prepare("INSERT INTO chemicals (c_id,rfid,name,row,col,presence) VALUES (1,'rfidnacl','NaCl',1,1,1),(2,'rfidnaoh','NaOH',1,2,1)");
+//     //for (let i = 0; i < 10; i++) {
+//     //    stmt.run("Ipsum " + i);
+//     //}
+//     //stmt.finalize();
 
-    //db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-      //  console.log(row.id + ": " + row.info);
-    //});
-});
+//     //db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+//       //  console.log(row.id + ": " + row.info);
+//     //});
+// });
 
-db.close();
+// db.close();
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
@@ -31,7 +31,13 @@ app.use(express.static("public"));
 app.use(cors()); 
 
 app.get('/', (req, res) => {
-  res.render("index");
+  const sql = "SELECT * FROM chemicals;";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.render('index', { model: rows });
+    });
 })
 
 app.post('/', (req, res) => {
