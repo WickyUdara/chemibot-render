46451db8fef4c306ec741b046945b559d998896d
Udara Wickramasinghe <65730273+WickyRR@users.noreply.github.com>
2024-06-30 21:00:00 +0530

    inventory

--- app.js
+++ app.js
@@ -11,39 +11,54 @@ const port = 8000;
 
 
 
-// db.serialize(() => {
-//     db.run("CREATE TABLE chemicals (c_id int NOT NULL,rfid VARCHAR(64),name VARCHAR(64),row INT(1),col INT(1),presence INT(1),PRIMARY KEY(c_id))");
-//     db.run("CREATE TABLE selected_items (c_id int(3),PRIMARY KEY(c_id))");
-
-//     const stmt = db.prepare("INSERT INTO chemicals (c_id,rfid,name,row,col,presence) VALUES (1,'rfidnacl','NaCl',1,1,1),(2,'rfidnaoh','NaOH',1,2,1)");
-//     //for (let i = 0; i < 10; i++) {
-//     //    stmt.run("Ipsum " + i);
-//     //}
-//     //stmt.finalize();
-
-//     //db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-//       //  console.log(row.id + ": " + row.info);
-//     //});
-// });
-
-// db.close();
 //db.serialize(() => {
-  //db.run(`CREATE TABLE users (
-  //  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  //  username TEXT UNIQUE,
-  //  password TEXT,
-  //  isAdmin BOOLEAN
-  //)`);
+//  db.run("CREATE TABLE chemicals (c_id INT NOT NULL, rfid VARCHAR(64), name VARCHAR(64), mfd TEXT, exd TEXT, row INT, col INT, presence INT, PRIMARY KEY(c_id))");
+//  db.run("CREATE TABLE selected_items (c_id INT, PRIMARY KEY(c_id))");
+
+// const stmt = db.prepare("INSERT INTO chemicals (c_id, rfid, name, mfd, exd, row, col, presence) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+//  stmt.run(1, 'rfidnacl', 'NaCl', '2024-05-26', '2024-05-26', 1, 1, 1);
+//  stmt.run(2, 'rfidnaoh', 'NaOH', '2024-05-26', '2024-05-26', 1, 2, 1);
+//  stmt.finalize();
+
+  // Ensure the 'lorem' table exists if you want to query from it
+  // db.run("CREATE TABLE lorem (info TEXT)");
+
+//  db.each("SELECT c_id AS id, name FROM chemicals", (err, row) => {
+//      if (err) {
+//          console.error(err);
+//      } else {
+//          console.log(row.id + ": " + row.name);
+//      }
+//  });
+//});
+
+//db.serialize(() => {
+//  db.run(`CREATE TABLE users (
+//    id INTEGER PRIMARY KEY AUTOINCREMENT,
+//    username TEXT UNIQUE,
+//    password TEXT,
+//    isAdmin BOOLEAN
+//  )`);
 
   // Create a default admin user
-  //const stmt = db.prepare(`INSERT INTO users (username, password, isAdmin) VALUES (?, ?, ?)`);
-  //const saltRounds = 10;
-  //const adminPassword = 'admin'; // Change this to a more secure password
-  //bcrypt.hash(adminPassword, saltRounds, (err, hash) => {
-  //  if (err) throw err;
-  //  stmt.run('admin', hash, true); // Provide the values for username, password, and isAdmin
-  //  stmt.finalize();
-  //});
+//  const stmt = db.prepare(`INSERT INTO users (username, password, isAdmin) VALUES (?, ?, ?)`);
+//  const saltRounds = 10;
+//  const adminPassword = 'admin'; // Change this to a more secure password
+//  bcrypt.hash(adminPassword, saltRounds, (err, hash) => {
+//   if (err) throw err;
+//    stmt.run('admin', hash, true); // Provide the values for username, password, and isAdmin
+//    stmt.finalize();
+//  });
+  // Create a  user
+//  const stmt2 = db.prepare(`INSERT INTO users (username, password, isAdmin) VALUES (?, ?, ?)`);
+  
+//  const userPassword = 'lab-user'; // Change this to a more secure password
+//  bcrypt.hash(userPassword, saltRounds, (err, hash) => {
+//    if (err) throw err;
+//    stmt2.run('lab-user', hash, false); // Provide the values for username, password, and isAdmin
+//    stmt2.finalize();
+//  });
 //});
 // set the view engine to ejs
 app.use(session({
@@ -86,21 +101,24 @@ app.post('/login', (req, res) => {
       bcrypt.compare(password, user.password, (err, result) => {
         if (result) {
           req.session.user = { id: user.id, username: user.username, isAdmin: user.isAdmin };
-          res.redirect('/dashboard');
+          console.log(user.isAdmin);
+          if(user.isAdmin==1){
+            res.redirect('/admin-dashboard');
+          }else{
+            res.redirect('/dashboard');
+          }
         } else {
           res.send('Invalid username or password');
         }
-      });
+      })
     } else {
       res.send('Invalid username or password');
     }
   });
 });
 
-app.get('/admin', isAuthenticated, isAdmin, (req, res) => {
-  res.send('Welcome to the admin panel, ' + req.session.user.username);
-});
-app.get('/dashboard',isAuthenticated, isAdmin, (req, res) => {
+
+app.get('/dashboard',isAuthenticated,  (req, res) => {
   const sql = "SELECT * FROM chemicals;";
     db.all(sql, [], (err, rows) => {
         if (err) {
@@ -108,6 +126,15 @@ app.get('/dashboard',isAuthenticated, isAdmin, (req, res) => {
         }
         res.render('index', { model: rows,username: req.session.user.username });
     });
+})
+app.get('/admin-dashboard',isAuthenticated, isAdmin, (req, res) => {
+  const sql = "SELECT * FROM chemicals;";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        res.render('admin',{ model: rows,username: req.session.user.username });
+      });
 })
 app.get('/logout', (req, res) => {
   req.session.destroy();
